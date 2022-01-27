4343a9ccea77c9d93d8042e37d7993fbaea1fe98
Julian Tao <julian.tao@asu.edu>
2022-01-27 16:32:33 -0700

    Add cards R code (WIP)

---
+++ tools/icon_cards.R
@@ -0,0 +1,24 @@
+library(htmltools)
+library(yaml)
+
+card <- function(x) {
+  div(
+    class = "card",
+    div(
+      class = "card-body",
+      div(class = "card-title", tags$h4(x$title)),
+      ifelse(!is.null(x$subtitle),
+        tagList(div(class = "card-subtitle text-muted", tags$h6(HTML(x$subtitle)))),
+        tagList()
+      ),
+      tags$ul(tagList(lapply(x$links, function(link) {
+        tags$li(tags$a(href = link[[2]], link[[1]]))
+      })))
+    )
+  )
+}
+
+link_cards <- function(file) {
+  cards <- yaml::yaml.load_file(file)
+  div(class = "link-cards", tagList(lapply(cards, card)))
+}
